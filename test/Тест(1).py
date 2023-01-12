8777e964e508f349fad52f32109beb9d4334945c
pumpkinVO <121223507+pumpkinVO@users.noreply.github.com>
2023-01-12 16:42:53 +0800

    Add files via upload

---
+++ test/Тест(1).py
@@ -0,0 +1,6 @@
+b=int(input("Введите число клетки"))
+w=int(input("Введите второе число клетки"))
+if b > w and b < w:
+    print('NO')
+elif b == w:
+    print('YES')
---
+++ test/Тест(3).py
@@ -0,0 +1,4 @@
+f=int(input("Введите возраст претендента"))
+if f < 10 or f == 10 and f < 15 or f == 15:
+    print('YES')
+else: print('NO')
